d76526072c3b2dea6cb9dfda74d4cee357d273af
Asaid15k <asaidshaikh84@gmail.com>
2026-05-04 11:37:07 +0530

    Remove all files

--- backend/database/create_tables.py
+++
@@ -1,9 +0,0 @@
-from .models import Base
-from .db_config import engine
-
-def create_all_tables():
-    Base.metadata.create_all(bind=engine)
-    print("All database tables created successfully!")
-
-if __name__ == "__main__":
-    create_all_tables()
